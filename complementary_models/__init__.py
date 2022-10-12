75f37b778efe32f40168acb6dde3bccc06b71898
HarryMao1011 <harrymao1011@gmail.com>
2022-10-11 21:55:48 -0400

    add linear model

--- complementary_models/__init__.py
+++ complementary_models/__init__.py
@@ -4,7 +4,7 @@ from .parallel_model import MultiVI_Parallel
 from .REGISTRY_KEYS import REGISTRY_KEYS
 from .utils import torch_dist2, torch_kernel, torch_infer_nonsta_dir
 # from .ScoreUtils import dist2, kernel, pdinv
-# from .infer_nonsta_dir import infer_nonsta_dir
+from .infer_nonsta_dir import infer_nonsta_dir
 from .HALOVI import HALOVI, HALOVAE
 from .HALOVI_Concat import HALOVAECAT, HALOVICAT
 from .HALOVI_Concat_stronger import HALOVAECAT2, HALOVICAT2
@@ -12,3 +12,5 @@ from .HALO_2en2de import HALOVAECAT3, HALOVICAT3
 from ._HALO_VAER import HALOVAER
 from ._HALO_VIR import HALOVIR
 from .data_utils import _get_latent_adata_type
+from ._HALO_LDVAER import HALOLDVAER
+from ._HALO_LDVIR import HALOLDVIR
